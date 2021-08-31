34c856a25df34caf1aae524f2a3dc7308a0fca8b
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-08-31 17:55:19 +0530

    removed unwanted stuff

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,19 +1,20 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/react";
 
-import theme from '../theme'
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
+    <ThemeProvider theme={theme}>
       <ColorModeProvider
         options={{
           useSystemColorMode: true,
         }}
       >
+        <CSSReset />
         <Component {...pageProps} />
       </ColorModeProvider>
-    </ChakraProvider>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
